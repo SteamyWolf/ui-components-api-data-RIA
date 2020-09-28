17fa9fdeba303f248e71d51c88060f75c3b4cac4
Wyatt <wyatthunterallan@gmail.com>
2020-09-28 17:23:13 -0600

    Cards displayed correctly

---
+++ src/Amiibo.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 445,
+      margin: 10
+    },
+    action: {
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 15,
+    },
+    media: {
+      height: 200,
+      width: '100%',
+      backgroundSize: 'contain'
+    },
+    ul: {
+        margin: '0 auto',
+        paddingLeft: 0,
+        maxWidth: 1200,
+        listStyleType: 'none',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    li: {
+        maxWidth: 360
+    },
+    button: {
+        textDecoration: 'none'
+    }
+  });
+
+
+const Amiibo = () => {
+    const classes = useStyles();
+
+    const [amiiboData, setAmiiboData] = useState({
+        loading: false,
+        data: []
+    })
+
+    const fetchAmiibo = () => {
+        setAmiiboData({ loading: true });
+        axios.get('https://www.amiiboapi.com/api/amiibo/?character=link')
+            .then(res => {
+                console.log(res)
+                setAmiiboData({
+                    loading: false,
+                    data: res.data.amiibo
+                })
+                console.log(amiiboData.data)
+            })
+            .catch((error) => console.log(error))
+    }
+
+    useEffect(() => {
+        fetchAmiibo();
+    }, [])
+
+    let link = null;
+    if (amiiboData.loading === false) {
+        return link = (
+            <ul className={classes.ul}>
+                { amiiboData.data.map(el =>
+                    <li className={classes.li} key={el.tail}>
+                        <Card className={classes.root}>
+                            <CardActionArea className={classes.action}>
+                                <CardMedia
+                                    className={classes.media}
+                                    image={el.image}
+                                    title="Link: The Hero of Hyrule">
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {el.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Release date in North America: {el.release.na}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    <a className={classes.button} href="#">Learn More</a>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </li>     
+                    )
+                }
+            </ul>
+        )
+    }
+
+//MAIN RETURN STATEMENT
+    return (
+        <div>
+            <LinearProgress color="secondary" />
+            {link}
+        </div>
+    )
+}
+
+export default Amiibo
--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Amiibo from './Amiibo';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 1200,
+    margin: '0 auto',
+  },
+  img: {
+    width: '100%'
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-     <p>Testing</p>
+      <header className={classes.root}>
+        <img className={classes.img} src="https://i.pinimg.com/originals/ed/96/e5/ed96e5b13349be8bec8cd09a7656c580.jpg"></img>
+      </header>
+     <Amiibo />
     </div>
   );
 }
