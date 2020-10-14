7ecd8a11f991f6828df02ad09eb98c9bc991dc2d
Wyatt <wyatthunterallan@gmail.com>
2020-10-14 14:34:35 -0600

    Added state and formik

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
 import { AmiiboContextProvider } from './contexts/AmiiboContext';
-import Toolbar from './components/Header';
+import Header from './components/Header';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Hero from './components/Hero';
 import Amiibo from './components/Amiibo';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 1200,
-    margin: '0 auto',
-  },
-  img: {
-    width: '100%'
-  }
-});
-
 function App() {
-  const classes = useStyles();
   return (
     <AmiiboContextProvider>
+      <Header />
       <Hero />
-      <Toolbar />
         <Switch>
           <Route path='/login' component={Login} />
           <Route path='/signup' component={Signup} />
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Formik } from 'formik';
+import { Link } from 'react-router-dom';
+
+
+const useStyles = makeStyles({
+    main: {
+    },
+    form: {
+        margin: '0 auto',
+        maxWidth: 400,
+    },
+    innerDiv: {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '10px 0 30px'
+    },
+    buttonDiv: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    textField: {
+        marginBottom: 10,
+    }
+})
 
 const Signup = () => {
+    const classes = useStyles();
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        passwordMatch: ''
+    });
+
     return (
-        <div>
-            <p>Signup</p>
-        </div>
+        <Formik initialValues={{ ...formData }} onSubmit={(data, { resetForm }) => {
+            console.log(data)
+            setFormData({
+                email: data.email,
+                password: data.password,
+                passwordMatch: data.passwordMatch
+            })
+            console.log(formData)
+            resetForm();
+            }}>
+            {({ values, handleChange, handleBlur, handleSubmit }) => (
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <div className={classes.innerDiv}>
+                        <h2>Signup:</h2>
+                        <p>Please enter you email and password</p>
+                        <TextField className={classes.textField} value={values.email} onChange={handleChange} onBlur={handleBlur} name="email" label="Email" helperText="Must be a valid email" type="email" required/>
+                        <TextField className={classes.textField} value={values.password} onChange={handleChange} onBlur={handleBlur} name="password" error={values.passwordMatch === values.password ? false : true} label="Password" helperText="Password must match" type="password" required/>
+                        <TextField className={classes.textField} value={values.passwordMatch} onChange={handleChange} onBlur={handleBlur} name="passwordMatch" error={values.passwordMatch === values.password ? false : true} label="Password-match" helperText="Password must match" type="password" required/>
+                        <div className={classes.buttonDiv}>
+                            <Button color="primary" variant="contained" disabled={values.passwordMatch === values.password ? false : true || values.password === '' ? true : false || values.passwordMatch === '' ? true : false || values.email === '' ? true : false} type="submit" >
+                                Submit
+                            </Button>
+                        </div>
+                    </div>
+                </form>
+            )}
+        </Formik>
     )
 }
 
